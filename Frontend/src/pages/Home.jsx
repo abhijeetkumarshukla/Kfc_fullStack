20521d8339e0295de81fbee10fc0fa594005b01d
abhijeetkumarshukla <shuklaabhijeet510@gmail.com>
2024-12-08 21:48:13 +0530

    slider1

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { Box, Text } from "@chakra-ui/react"
 import Navbar from "../components/navbar/Navbar"
-import { MdOutlineLocationOn } from "react-icons/md"
+import { MdLocationOn } from "react-icons/md";
+ 
+import PauseOnHover from "../components/slider/Slider1";
  
 
  
@@ -10,10 +12,14 @@ const Home = () => {
     <Box>     
       <Navbar/>
       <Box gap={'3'}  display={'flex'} justifyContent={'center'} bgColor={'black'} p={'17px'} color={'white'}>
-      <Box    fontSize={'2xl'}  > <MdOutlineLocationOn /> </Box>
-      <Text fontSize={'.1xl'} fontWeight={'400'}>Select a store to start your order.</Text>
-       
-      </Box>
+      <Box    fontSize={'1.3rem'}  >   <MdLocationOn  style={{color:"#E4002B"}} /> </Box>
+      <Text fontSize={'15px'} fontWeight={'400'}>Select a store to start your order.</Text>
+        </Box>
+
+
+    {/* Slider 1 */}
+    <PauseOnHover/>
+
     </Box>
   )
 }
